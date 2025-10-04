9a63a00d9ebb7931a8755843c5599cba97c96446
Tracnac <tracnac@devmobs.fr>
2025-10-04 10:39:01 +0200

    Add a switch for frequency controlled or not

--- cpuvm8.c
+++ cpuvm8.c
@@ -4,8 +4,35 @@
 double freq_mhz = 16.0;
 
 int main(int argc, char *argv[]) {
+#if FULL_SPEED
     (void)argc; (void)argv;
+    CPU cpu;
+    initCPU(&cpu);
 
+    // Programme de test (boucle simple)
+    cpu.memory[0] = OPCODE_LDA;
+    cpu.memory[1] = MODE_IMM;
+    cpu.memory[2] = 42;
+    cpu.memory[3] = OPCODE_B;
+    cpu.memory[4] = COND_AL;
+    cpu.memory[5] = 0;  // Jump à 0 (boucle infinie)
+
+    struct timespec start, now;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    printf("Benchmark: %d instructions...\n", INSTR_COUNT);
+    for (int i = 0; i < INSTR_COUNT; i++) {
+        cpu_step(&cpu);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    double total_elapsed = (now.tv_sec - start.tv_sec) + (now.tv_nsec - start.tv_nsec) / 1e9;
+    double mips = (double)INSTR_COUNT / (total_elapsed * 1e6);
+
+    printf("--------------------------------------------------\n");
+    printf("Executed %d instructions in %.5f seconds\n", INSTR_COUNT, total_elapsed);
+    printf("Performance: %.2f MIPS (Millions of Instructions Per Second)\n", mips);
+    printf("--------------------------------------------------\n");
+#else
     if (argc > 1) {
         freq_mhz = atof(argv[1]);
         if (freq_mhz < 0.01) freq_mhz = 0.01;
@@ -22,8 +49,9 @@ int main(int argc, char *argv[]) {
     cpu.memory[4] = COND_AL;
     cpu.memory[5] = 0;  // Jump à 0 (boucle infinie)
 
-    printf("Simulating CPU at %.2f MHz\n", freq_mhz);
     printf("Benchmark: %d instructions...\n", INSTR_COUNT);
+    printf("Benchmark: %d instructions...\n", INSTR_COUNT);
+    printf("Simulating CPU at %.2f MHz\n", freq_mhz);
 
     struct timespec start, now;
     clock_gettime(CLOCK_MONOTONIC, &start);
@@ -55,4 +83,5 @@ int main(int argc, char *argv[]) {
     printf("Simulated frequency: %.2f MHz\n", freq_mhz);
     printf("Performance: %.2f MIPS (Millions of Instructions Per Second)\n", mips);
     printf("--------------------------------------------------\n");
+#endif
 }
